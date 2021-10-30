6190030a4b76576801a144b5e08e71c4c72f6b9e
Anna Bespalko <anbesp365@gmail.com>
2021-10-30 22:30:05 +0300

    add a new java program

---
+++ src/program/FirstProgram.java
@@ -0,0 +1,30 @@
+package program;
+
+import java.util.Scanner;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        double c = 0;
+        char operator;
+        Scanner scanObject = new Scanner(System.in);
+        System.out.println("Введите первую цифру");
+        a = scanObject.nextInt();
+        System.out.println("Введите вторую цифру");
+        b = scanObject.nextInt();
+        System.out.println("Какое действие?");
+        operator = scanObject.next().charAt(0);
+
+        switch (operator) {
+            case '+': c = a + b; break;
+            case '-': c = a - b; break;
+            case '*': c = a * b; break;
+            case '/': c = a / b; break;
+            default:
+                System.out.println("Введите корректный оператор (+, - , * , /)");break;
+        }
+        System.out.println(a+" "+operator+" "+b+" = "+c);
+    }
+}
+
